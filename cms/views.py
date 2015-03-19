d8857fae4a1d2d5b40b6e3535c777ad2a1a0b613
rebeca <rebeca@toshiba-rebeca.(none)>
2015-03-19 15:39:31 +0100

    Servidor con GET y PUT

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+from models import Pages
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
+
+@csrf_exempt
+def handler(request, recurso):
+    if request.method == "GET":
+        try:
+            fila = Pages.objects.get(name=recurso)
+            return HttpResponse(fila.name + ": " + fila.page)
+        except Pages.DoesNotExist:
+            return HttpResponseNotFound("Page not found: " + recurso)
+    if request.method == "PUT":
+            nuevo = Pages(name=recurso, page=request.body)
+            nuevo.save()
+            return HttpResponse("Saved Page, check it with GET")
+    else:
+        return HttpResponseNotFound("Method not found: " + request.method)
